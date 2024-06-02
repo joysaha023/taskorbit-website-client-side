a664510fa427ea58899bc56f93cf89e1a2add091
Joy <github.joysaha@gmail.com>
2024-06-02 09:42:49 +0600

    navbar create and added logo and button

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
+import { MdOndemandVideo } from "react-icons/md";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h2>Navbar</h2>
+  return (
+    <div className="max-w-6xl mx-auto">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <Link to="/" ><img className="w-full h-[40px] md:h-[60px]" src={logo} alt="" /></Link>
         </div>
-    );
+        <div className="navbar-end hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn"><MdOndemandVideo /> Demo</a>
+          <a className="btn">Login</a>
+          <a className="btn">Register</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
