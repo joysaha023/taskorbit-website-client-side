4d27c0b84ec67234b03607a44a05d6b967f1c6ef
Joy <github.joysaha@gmail.com>
2024-06-02 08:38:31 +0600

    route setup and navbar and footer folder created

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from '../Components/Shared/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
-            <h2>Hello World</h2>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main";
+import Home from "../Pages/Home/Home";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+    ]
   },
 ]);
 
