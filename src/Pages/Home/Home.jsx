f77015d4d46976ca0af3c3eb7297a6c4e15d5dc3
Joy <github.joysaha@gmail.com>
2024-06-03 11:15:09 +0600

    how it work section is complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Features from './Features/Features';
+import Works from './Works/Works';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Features></Features>
+            <Works></Works>
         </div>
     );
 };
---
+++ src/Pages/Home/Works/Works.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { CgGift } from "react-icons/cg";
+import { MdOutlineAddTask } from "react-icons/md";
+import { VscAccount } from "react-icons/vsc";
+
+const Works = () => {
+  return (
+    <div className="max-w-6xl mx-auto px-2">
+      <div className="text-center my-6">
+        <h2 className="text-3xl md:text-4xl font-bold">How It Works</h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="card card-compact w-full bg-base-100 border shadow-xl">
+          <figure className="p-8">
+            <VscAccount className="text-7xl" />
+          </figure>
+          <div className="card-body ">
+            <h2 className="card-title mx-auto">Register</h2>
+            <p className="mx-auto text-center">
+              Create a valid account as a your choice role worker or task creator and you can complete task
+            </p>
+          </div>
+        </div>
+        <div className="card card-compact  w-full bg-base-100 border shadow-xl">
+          <figure className="p-8">
+            <MdOutlineAddTask className="text-7xl" />
+          </figure>
+          <div className="card-body ">
+            <h2 className="card-title mx-auto">Complete Tasks</h2>
+            <p className="mx-auto text-center">
+              You can work on our task and complete these task on time You get more coin. 
+            </p>
+          </div>
+        </div>
+        <div className="card card-compact  w-full bg-base-100 border shadow-xl">
+          <figure className="p-8">
+            <CgGift className="text-7xl" />
+          </figure>
+          <div className="card-body ">
+            <h2 className="card-title mx-auto">Earn Rewards</h2>
+            <p className="mx-auto text-center">
+              You can work on our weakly and daily task thats and complete get money with extra reward.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
