094fb26ffbd699b90c814f699d6967c3bc0110b4
Joy <github.joysaha@gmail.com>
2024-06-08 21:45:33 +0600

    my submission route created and show data in tabuler formate

--- src/Pages/Dashboard/JobDetails/JobDetails.jsx
+++ src/Pages/Dashboard/JobDetails/JobDetails.jsx
@@ -2,8 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
 import { useParams } from "react-router-dom";
+import useAuth from "../../../Hooks/useAuth";
 
 const JobDetails = () => {
+  const { user } = useAuth();
   const { id } = useParams();
 
   const { data } = useQuery({
@@ -13,6 +15,48 @@ const JobDetails = () => {
       return res.data;
     },
   });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const submission_details = form.submit_details.value;
+    const task_id = data?._id;
+    const task_title = data?.task_title;
+    const task_details = data?.task_detail;
+    const task_img_url = data?.task_img_url;
+    const payable_amount = data?.payable_amount;
+    const worker_email = user?.email;
+    const worker_name = user?.displayName;
+    const creator_email = data?.creator_email;
+    const creator_name = data?.creator_name;
+    const current_date = new Date();
+    const status = "Pending";
+    const Submission_Info = {
+      submission_details,
+      task_id,
+      task_title,
+      task_details,
+      task_img_url,
+      payable_amount,
+      worker_email,
+      worker_name,
+      creator_email,
+      creator_name,
+      current_date,
+      status
+    };
+    
+
+    fetch('http://localhost:5000/submitDetails', {
+        method: 'POST',
+        headers: {
+            'content-type' : 'application/json',
+        },
+        body: JSON.stringify(Submission_Info)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  };
 
   return (
     <div>
@@ -30,14 +74,21 @@ const JobDetails = () => {
           </figure>
           <div className="card-body">
             <h2 className="card-title">{data?.task_title}</h2>
-            <p>{data?.task_detail}</p>
+            <p><span className="font-bold">Task Detail :</span> {data?.task_detail}</p>
+            <p><span className="font-bold">Submission Info : </span>{data?.submission_info}</p>
             <p className="font-bold">Submission Details:</p>
-            <form action="">
-             <div> <textarea
-                className="textarea w-full textarea-bordered"
-                placeholder="Submit your work Details..."
-              ></textarea></div>
-              <button className="btn btn-block text-white btn-primary mt-4">Submit</button>
+            <form onSubmit={handleSubmit}>
+              <div>
+                {" "}
+                <textarea
+                  className="textarea w-full textarea-bordered"
+                  name="submit_details"
+                  placeholder="Submit your work Details..."
+                ></textarea>
+              </div>
+              <button className="btn btn-block text-white btn-primary mt-4">
+                Submit
+              </button>
             </form>
           </div>
         </div>
--- src/Pages/Dashboard/WorkerSubmission/WorkerSubmission.jsx
+++ src/Pages/Dashboard/WorkerSubmission/WorkerSubmission.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React from "react";
+import useAuth from "../../../Hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const WorkerSubmission = () => {
-    return (
-        <div>
-            <h2 className='text-2xl'>My Submission</h2>
+  const { user } = useAuth();
+
+  const { data } = useQuery({
+    queryKey: ["taskDetails"],
+    queryFn: async () => {
+      const res = await axios.get(
+        `http://localhost:5000/submitData/${user.email}`
+      );
+      return res.data;
+    },
+  });
+
+  return (
+    <div>
+      <div>
+        <h2 className="text-2xl text-center mb-4 font-bold">
+          My All Submission
+        </h2>
+      </div>
+      <div>
+        <div className="overflow-x-auto px-4">
+          <table className="table border">
+            {/* head */}
+            <thead className="bg-slate-300">
+              <tr>
+                <th>Task Id</th>
+                <th>Task Title</th>
+                <th>Amount</th>
+                <th>Status</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((item) =>  <tr key={item._id}>
+                <th>{item.task_id.slice(0,7)}</th>
+                <td>{item.task_title}</td>
+                <td>{item.payable_amount}</td>
+                <td>{item.status}</td>
+                <td>{item.current_date.slice(0,10)}</td>
+              </tr>)}
+             
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default WorkerSubmission;
