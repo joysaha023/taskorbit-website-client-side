f5baba2c91f7c233d3806a6436e6d7200d434bd5
Joy <github.joysaha@gmail.com>
2024-06-22 22:48:18 +0600

    functionality added

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,10 +1,57 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React from 'react';
 
 const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if(!stripe || !elements){
+            return
+        }
+
+        const card = elements.getElement(CardElement)
+
+        if(card === null){
+            return
+        }
+
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if(error){
+            console.log('payment error', error)
+        }
+        else{
+            console.log('payment method', paymentMethod)
+        }
+    }
+
     return (
-        <div>
-            
-        </div>
+        <form className='px-6' onSubmit={handleSubmit}>
+             <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: '16px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}
+      />
+      <button className='btn btn-primary my-6' type="submit" disabled={!stripe}>
+        Pay
+      </button>
+        </form>
     );
 };
 
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,17 +1,18 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
+import CheckoutForm from "./CheckoutForm";
 
-const stripePromise = loadStripe("");
+const stripePromise = loadStripe(import.meta.env.VITE_payment_gateway_pk);
 const Payment = () => {
   return (
     <div>
       <div>
-        <h2 className="text-2xl text-center mb-4 font-bold">Add Task</h2>
+        <h2 className="text-2xl text-center mb-4 font-bold">Make Payment</h2>
       </div>
       <div>
         <Elements stripe={stripePromise}> 
-
+            <CheckoutForm></CheckoutForm>
         </Elements>
       </div>
     </div>
